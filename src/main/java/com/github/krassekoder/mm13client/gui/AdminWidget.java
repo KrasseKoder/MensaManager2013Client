978c7b55065a2a04c53f6c0fb25e71372b86b107
danielp1000 <leinad.ffafp@gmail.com>
2013-07-11 20:54:36 +0200

    Added deletion to AdminWidget

--- src/main/java/com/github/krassekoder/mm13client/gui/AdminWidget.java
+++ src/main/java/com/github/krassekoder/mm13client/gui/AdminWidget.java
@@ -2,12 +2,16 @@ package com.github.krassekoder.mm13client.gui;
 
 import com.github.krassekoder.mm13client.network.Packet;
 import com.github.krassekoder.mm13client.network.Packet4Admin;
+import com.trolltech.qt.gui.QDialog;
+import com.trolltech.qt.gui.QDoubleSpinBox;
 import com.trolltech.qt.gui.QFormLayout;
 import com.trolltech.qt.gui.QGroupBox;
-import com.trolltech.qt.gui.QHBoxLayout;
+import com.trolltech.qt.gui.QInputDialog;
 import com.trolltech.qt.gui.QLabel;
 import com.trolltech.qt.gui.QLineEdit;
+import com.trolltech.qt.gui.QMessageBox;
 import com.trolltech.qt.gui.QPushButton;
+import com.trolltech.qt.gui.QSpinBox;
 import com.trolltech.qt.gui.QVBoxLayout;
 import com.trolltech.qt.gui.QWidget;
 import java.util.logging.Level;
@@ -22,12 +26,13 @@ public class AdminWidget extends QWidget
 {
     private QGroupBox newUser;
     private QGroupBox newEntry;
-    private QHBoxLayout qh1,qh2,qh3,qh4,qh5,qh6;
-    private QVBoxLayout qv1,qv2,qv3;
-    private QLineEdit username, password,productname,price,id,rights;
+    private QVBoxLayout qv1, qv2, qv3;
+    private QFormLayout fLa1, fLa2;
+    private QLineEdit username, password, productname;
+    private QSpinBox rights, id;
+    private QDoubleSpinBox price;
     private QLabel name1Label,name2Label,passwordLabel,priceLabel,IdLabel,rightsLabel;
-    private QPushButton enter1,enter2;
-    private QFormLayout loginLayout;
+    private QPushButton enter1,enter2, deleteUser, deleteProduct;
 
     public AdminWidget(QWidget qw)
  {
@@ -39,39 +44,39 @@ public class AdminWidget extends QWidget
      * This method sets up the User Interface of "AdminWidget" including
      * 'QGroupBox' and 'QPushButton'.
      */
-  private void setupUi(){
+    private void setupUi(){
+        setLayout(qv1=new QVBoxLayout());
 
-     setLayout(qv1=new QVBoxLayout());
-     qv1.addWidget(newUser = new QGroupBox(tr("New User"), this));
-     newUser.setLayout(qv2 = new QVBoxLayout(newUser));
-     qv2.addLayout(qh1 = new QHBoxLayout());
-     qh1.addWidget(name1Label = new QLabel(tr("Username:"), newUser));
-     qh1.addWidget(username = new QLineEdit(newUser));
-     qv2.addLayout(qh2 = new QHBoxLayout());
-     qh2.addWidget(passwordLabel = new QLabel(tr("Password: "), newUser));
-     qh2.addWidget(password = new QLineEdit(newUser));
+        qv1.addWidget(newEntry = new QGroupBox(tr("Product"), this));
+        newEntry.setLayout(qv3 = new QVBoxLayout(newEntry));
+        qv3.addLayout(fLa2 = new QFormLayout(newEntry));
+        fLa2.addRow(name2Label = new QLabel(tr("Name:"), newEntry), productname = new QLineEdit(newEntry));
+        fLa2.addRow(priceLabel = new QLabel(tr("Price:"), newEntry), price = new QDoubleSpinBox(newEntry));
+        price.setRange(0, Double.MAX_VALUE);
+        price.setSingleStep(0.01);
+        price.setSuffix(tr("$"));
+        fLa2.addRow(IdLabel = new QLabel(tr("Id:"), newEntry), id = new QSpinBox(newEntry));
+        id.setRange(0, Integer.MAX_VALUE);
+        qv3.addWidget(enter2 = new QPushButton(tr("Enter / Edit"), this));
 
-     qv2.addLayout(qh6 = new QHBoxLayout());
-     qh6.addWidget(rightsLabel = new QLabel(tr("Rights:      "), newEntry));
-     qh6.addWidget(rights = new QLineEdit(newEntry));
 
-     qv2.addWidget(enter1 = new QPushButton(tr("&Enter"), this));
+        qv1.addWidget(deleteProduct = new QPushButton(tr("Delete Product..."), this));
 
-     qv1.addWidget(newEntry = new QGroupBox(tr("New Entry"), this));
-     newEntry.setLayout(qv3 = new QVBoxLayout(newEntry));
-     qv3.addLayout(qh3 = new QHBoxLayout());
-     qh3.addWidget(name2Label = new QLabel(tr("Name:"), newEntry));
-     qh3.addWidget(productname = new QLineEdit(newEntry));
-     qv3.addLayout(qh4 = new QHBoxLayout());
-     qh4.addWidget(priceLabel = new QLabel(tr("Price: "), newEntry));
-     qh4.addWidget(price = new QLineEdit(newEntry));
-     qv3.addLayout(qh5 = new QHBoxLayout());
-     qh5.addWidget(IdLabel = new QLabel(tr("Id:     "), newEntry));
-     qh5.addWidget(id = new QLineEdit(newEntry));
-     qv3.addWidget(enter2 = new QPushButton(tr("&Enter"), this));
+        qv1.addWidget(newUser = new QGroupBox(tr("User"), this));
+        newUser.setLayout(qv2 = new QVBoxLayout(newUser));
+        qv2.addLayout(fLa1 = new QFormLayout(newUser));
+        fLa1.addRow(name1Label = new QLabel(tr("Username:"), newUser), username = new QLineEdit(newUser));
+        fLa1.addRow(passwordLabel = new QLabel(tr("Password:"), newUser), password = new QLineEdit(newUser));
+        fLa1.addRow(rightsLabel = new QLabel(tr("Rights:"), newUser), rights = new QSpinBox(newUser));
+        rights.setRange(1, Byte.MAX_VALUE);
+        qv2.addWidget(enter1 = new QPushButton(tr("Enter / Edit"), this));
 
-     enter1.clicked.connect(this,"SaveNewUser()");
-     enter2.clicked.connect(this,"SaveEntry()");
+        qv1.addWidget(deleteUser = new QPushButton(tr("Delete User..."), this));
+
+        enter1.clicked.connect(this,"SaveNewUser()");
+        enter2.clicked.connect(this,"SaveEntry()");
+        deleteProduct.clicked.connect(this,"deleteProduct()");
+        deleteUser.clicked.connect(this,"deleteUser()");
      }
 
   /**
@@ -80,6 +85,10 @@ public class AdminWidget extends QWidget
    */
   public void SaveEntry()
   {
+      if(productname.text().isEmpty()) {
+          QMessageBox.information(this, tr("Invalid Product"), tr("Productname must not be empty."));
+          return;
+      }
         try {
             Packet4Admin.instance.editProduct(id.text(), productname.text(), price.text());
         } catch(Packet.InvalidPacketException ex) {
@@ -88,7 +97,7 @@ public class AdminWidget extends QWidget
       System.out.println("Product(" + id.text() + "): " + productname.text() + " : " + price.text());
       productname.clear();
       id.clear();
-      price.clear();
+      price.setValue(0);
       MainWindow.instance.showFoodList();
   }
   /**
@@ -97,6 +106,10 @@ public class AdminWidget extends QWidget
    */
   public void SaveNewUser()
   {
+      if(username.text().isEmpty()) {
+          QMessageBox.information(this, tr("Invalid User"), tr("Username must not be empty."));
+          return;
+      }
         try {
             Packet4Admin.instance.editUser(username.text(), password.text(), rights.text());
         } catch(Packet.InvalidPacketException ex) {
@@ -105,8 +118,34 @@ public class AdminWidget extends QWidget
       System.out.println("User:" + username.text() + " " + password.text() + "(" + rights.text() + ")");
       username.clear();
       password.clear();
-      rights.clear();
+      rights.setValue(1);
   }
+
+  private void deleteUser() {
+      String name = QInputDialog.getText(this, tr("Delete User"), tr("Username:"));
+      if(name.isEmpty())
+          return;
+        try {
+            Packet4Admin.instance.editUser(name, "", "0");
+        } catch(Packet.InvalidPacketException ex) {
+            Logger.getLogger(AdminWidget.class.getName()).log(Level.SEVERE, null, ex);
+        }
+  }
+
+    private void deleteProduct() {
+        QInputDialog d = new QInputDialog(this);
+        d.setInputMode(QInputDialog.InputMode.IntInput);
+        d.setIntRange(0, Integer.MAX_VALUE);
+        d.setWindowTitle(tr("Delete Product"));
+        d.setLabelText(tr("Id:"));
+        d.exec();
+        if(d.result() != QDialog.DialogCode.Accepted.value())
+            return;
+        try {
+            Packet4Admin.instance.editProduct(d.intValue() + "", "", "0");
+        } catch(Packet.InvalidPacketException ex) {
+            Logger.getLogger(AdminWidget.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        MainWindow.instance.showFoodList();
+    }
 }
-
-
